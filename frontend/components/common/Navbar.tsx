8ce0d4f51e06ac6d8e898703226f0c57482760dc
김형진 <rlagudwls3469@gmail.com>
2024-04-05 15:54:46 +0900

    ref: navbar design (dropdown)

--- frontend/components/common/Navbar.tsx
+++ frontend/components/common/Navbar.tsx
@@ -12,6 +12,10 @@ import logo from '@/public/assets/imgs/dalkak_logo.png';
 import authStore from '@/store/authStore';
 import memberStore from '@/store/memberStore';
 import useRefrigeratorStore from '@/store/refrigeratorStore';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { useRouter } from 'next/navigation';
+import { styled } from '@mui/material';
 
 // import memberStore from '@/store/memberStore';
 
@@ -22,7 +26,8 @@ export default function Navbar() {
   const clearTokens = authStore((state) => state.clearTokens);
   const [userNickname, setUserNickname] = useState('');
   const { clearRefgMemo } = useRefrigeratorStore();
-
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
   const LogoutFunction = async () => {
     try {
       const response = await Logout();
@@ -42,58 +47,89 @@ export default function Navbar() {
   }, []);
 
   return (
-    <div className={styles.navbar}>
-      <div className={styles.left}>
-        <div className={styles.logo}>
-          <Link href="/">
-            <Image src={logo} width={140} height={100} alt="logo" />
-          </Link>
-        </div>
-        <div className={styles.pages}>
-          <Link href={isLoggedIn ? '/cocktail' : '/oauth'}>
-            <div
-              className={pathName?.startsWith('/cocktail') ? styles.active : ''}
-            >
-              칵테일 목록
-            </div>
-          </Link>
+    <>
+      <div className={styles.navbar}>
+        <div className={styles.left}>
+          <div className={styles.logo}>
+            <Link href="/">
+              <Image src={logo} width={140} height={100} alt="logo" />
+            </Link>
+          </div>
+          <div className={styles.pages}>
+            <Link href={isLoggedIn ? '/cocktail' : '/oauth'}>
+              <div
+                className={
+                  pathName?.startsWith('/cocktail') ? styles.active : ''
+                }
+              >
+                칵테일 목록
+              </div>
+            </Link>
 
-          <Link href={isLoggedIn ? '/storage' : '/oauth'}>
-            <div
-              className={pathName?.startsWith('/storage') ? styles.active : ''}
-            >
-              냉장고
-            </div>
-          </Link>
+            <Link href={isLoggedIn ? '/storage' : '/oauth'}>
+              <div
+                className={
+                  pathName?.startsWith('/storage') ? styles.active : ''
+                }
+              >
+                냉장고
+              </div>
+            </Link>
+          </div>
+        </div>
+        <div className={styles.center}>
+          <div className={styles.fix}>
+            현재 가장 인기 있는 칵테일&nbsp;&nbsp;|&nbsp;&nbsp;
+          </div>
+          <div>
+            <NavbarTopRank />
+          </div>
+        </div>
+        <div className={styles.right}>
+          {isLoggedIn ? (
+            <>
+              <div className={styles.nickname}>{userNickname}</div>님 반가워요!
+              <button
+                className={styles.dropdown}
+                type="button"
+                onClick={() => {
+                  setIsOpen(!isOpen);
+                }}
+              >
+                {isOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+              </button>
+              {/* <button
+                className={styles.btn}
+                type="button"
+                onClick={() => LogoutFunction()}
+              >
+                로그아웃
+              </button> */}
+            </>
+          ) : (
+            <div className={styles.needToLogin}>로그인이 필요해요</div>
+          )}
+          {/* <Link href="/member">
+            <HomeIcon />
+          </Link> */}
         </div>
       </div>
-      <div className={styles.center}>
-        <div className={styles.fix}>
-          현재 가장 인기 있는 칵테일&nbsp;&nbsp;|&nbsp;&nbsp;
+      {isOpen && (
+        <div className={styles.info}>
+          <button
+            type="button"
+            onClick={() => {
+              router.push('/member');
+              setIsOpen(false);
+            }}
+          >
+            회원정보
+          </button>
+          <button type="button" onClick={() => LogoutFunction()}>
+            로그아웃
+          </button>
         </div>
-        <div>
-          <NavbarTopRank />
-        </div>
-      </div>
-      <div className={styles.right}>
-        {isLoggedIn ? (
-          <>
-            <div className={styles.nickname}>{userNickname}</div>님 반가워요!
-            <button
-              className={styles.btn}
-              type="button"
-              onClick={() => LogoutFunction()}
-            >
-              로그아웃
-            </button>
-          </>
-        ) : (
-          <div className={styles.login}>로그인이 필요해요</div>
-        )}
-        <Link href="/member">
-          <HomeIcon />
-        </Link>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
