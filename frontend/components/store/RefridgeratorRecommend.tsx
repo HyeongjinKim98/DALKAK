98975fc9fae01f27c35042348d32455ccf080953
유현지 <ccamy0915@naver.com>
2024-04-05 17:58:33 +0900

    design: Modify refrigerator CSS(ing)

--- frontend/components/store/RefridgeratorRecommend.tsx
+++ frontend/components/store/RefridgeratorRecommend.tsx
@@ -3,7 +3,8 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './RefridgeratorRecommend.module.scss';
-import refrigeratorRecommemd from '@/public/assets/imgs/refrigeratorRecommend.png';
+import fireworks from '@/public/assets/imgs/fireworks.png';
+import recommendTools from '@/public/assets/imgs/recommendTools.png';
 
 export default function RefridgeratorRecommend() {
   return (
@@ -11,9 +12,16 @@ export default function RefridgeratorRecommend() {
       <div className={styles.container}>
         <div className={styles['image-wrapper']}>
           <Image
-            src={refrigeratorRecommemd}
+            src={fireworks}
+            width={1000}
+            alt="냉장고 추천 hover"
+            className={styles.firework}
+          />
+          <Image
+            src={recommendTools}
             width={1000}
             alt="냉장고 추천 이미지"
+            className={styles.tools}
           />
         </div>
         <div className={styles['text-container']}>
